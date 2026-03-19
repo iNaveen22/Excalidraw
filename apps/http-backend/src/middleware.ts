81a0349ed4bda0d781ea11bba4b69e27213fa5f7
Naveen kumar <naveenkumar000568@gmail.com>
2026-03-20 03:39:29 +0530

    fixes module

--- apps/http-backend/src/middleware.ts
+++ apps/http-backend/src/middleware.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from 'jsonwebtoken'
-import {JWT_SECRET} from "@repo/backend-common/config";
+import {JWT_SECRET} from "@repo/backend-common";
 import { AuthJwtPayload } from "./types/jwt";
 
 export function middleware(req: Request, res: Response, next: NextFunction){
